773e7d9b4bbc24f316ae0115fe0505b3f6c9f6f4
Christopher Gonzales <ccgonzales@gmail.com>
2019-06-28 13:35:53 -0400

    add ability to handle api errors

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,33 +1,61 @@
 import React from "react"
 import {Container, Divider, Segment} from "semantic-ui-react"
 
+const ErrorResults = props => {
+  const errorDesc = props.xmlDoc.getElementsByTagName("Description")[0]
+    .innerHTML
+  return (
+    <React.Fragment>
+      <h2>Error</h2>
+      <Divider />
+      <Segment>
+        <p>{errorDesc}</p>
+      </Segment>
+    </React.Fragment>
+  )
+}
+
 const Results = ({response}) => {
   let parser = new DOMParser()
   const doc = parser.parseFromString(response, "text/xml")
   console.log(doc)
 
+  const error = doc.getElementsByTagName("Error").length !== 0 ? true : false
+
+  let price,
+    zone = ""
+
+  if (!error) {
+    zone = doc.getElementsByTagName("Zone")[0].childNodes[0].nodeValue
+    if (doc.getElementsByTagName("CommercialRate").length !== 0) {
+      price = doc.getElementsByTagName("CommercialRate")[0].childNodes[0]
+        .nodeValue
+    } else if (doc.getElementsByTagName("Rate").length !== 0) {
+      price = doc.getElementsByTagName("Rate")[0].childNodes[0].nodeValue
+    }
+  }
+
   return (
     <React.Fragment>
       <Divider hidden />
       <Container>
-        <h2>Rate</h2>
-        <Divider />
         <Segment.Group horizontal>
-          <Segment>
-            <h2>
-              Price: $
-              {
-                doc.getElementsByTagName("CommercialRate")[0].childNodes[0]
-                  .nodeValue
-              }
-            </h2>
-          </Segment>
-          <Segment>
-            <h3>
-              Zone:{" "}
-              {doc.getElementsByTagName("Zone")[0].childNodes[0].nodeValue}
-            </h3>
-          </Segment>
+          {error ? (
+            <Segment>
+              <ErrorResults xmlDoc={doc} />
+            </Segment>
+          ) : (
+            <Segment>
+              <h2>Rate</h2>
+              <Divider />
+              <Segment>
+                <h2>Price: ${price}</h2>
+              </Segment>
+              <Segment>
+                <h3>Zone: {zone}</h3>
+              </Segment>
+            </Segment>
+          )}
         </Segment.Group>
       </Container>
     </React.Fragment>
